1b6bb50f1155b4bb87726f7d9aeed0c40d1e76e5
igoramf <igoraaf1@gmail.com>
2024-01-31 21:22:16 -0300

    fix: noob error again

--- back-end-falk/src/services/postServices/createPost.js
+++ back-end-falk/src/services/postServices/createPost.js
@@ -4,7 +4,8 @@ const {
     HTTP_CODE_OK,
     HTTP_CODE_UNAUTHORIZED,
     HTTP_CODE_BAD_REQUEST,
-    HTTP_CODE_INTERNAL_SERVER_ERROR
+    HTTP_CODE_INTERNAL_SERVER_ERROR,
+    HTTP_CODE_CREATED
   } = require("../../utils/httpStatus.js");
 
 async function createPost(req, res){
@@ -24,6 +25,7 @@ async function createPost(req, res){
         })
         
     } catch (error) {
+        console.log(error)
         return res.status(HTTP_CODE_INTERNAL_SERVER_ERROR).json({
             message: "Erro ao criar o post"
           });
