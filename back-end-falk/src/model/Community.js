62772acc1f8784dfd19c9ddf9885a2aeaad85674
igoramf <igoraaf1@gmail.com>
2024-02-20 19:38:53 -0300

    feat: community model

---
+++ back-end-falk/src/model/Community.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose")
+
+const communitySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    users: {
+        type: [Schema.Types.ObjectId],
+        ref: 'User',
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = model('Comment', commentSchema);
